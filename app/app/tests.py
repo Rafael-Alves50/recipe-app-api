eaaab509e5a0ffb1762247e72460d26c6a3d92d3
Rafael <rafael.alves50@gmail.com>
2024-03-07 10:16:02 -0300

    Fix Identation 3

--- app/app/tests.py
+++ app/app/tests.py
@@ -9,12 +9,12 @@ class CalcTests(SimpleTestCase):
     """Test the calc module."""
 
     def test_add_numbers(self):
-      """Test adding numbers together"""
-      res = calc.add(5, 6)
-      self.assertEqual(res, 11)
+        """Test adding numbers together"""
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-      """Test subtracting numbers."""
-      res = calc.subtract(15, 10)
+        """Test subtracting numbers."""
+        res = calc.subtract(15, 10)
 
-      self.assertEqual(res, 5)
+        self.assertEqual(res, 5)
